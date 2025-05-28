da832d5a357731e90fe5713c3870fbbd2bae2eff
dgonzale333 <davidgonzalezdiaz2305@gmail.com>
2025-05-28 08:48:47 -0500

    commit para guardar todo

--- src/main/java/com/FinZen/controllers/PresupController.java
+++ src/main/java/com/FinZen/controllers/PresupController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-
-
 @RestController
 @RequestMapping("/finzen/presupuesto")
 public class PresupController {
