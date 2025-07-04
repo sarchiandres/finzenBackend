00ba3b5f87906dca524a60f0d0fd42a90c62af2f
Julian Cortes <mmm808383@gmail.com>
2025-07-04 09:30:27 -0500

    Cambios en fzAplication3

--- src/main/java/com/FinZen/FinZenApplication.java
+++ src/main/java/com/FinZen/FinZenApplication.java
@@ -8,11 +8,15 @@ public class FinZenApplication {
 
 	public static void main(String[] args) {
 
+		System.out.println("======= INICIO: Verificación de variables de entorno =======");
+
 		setEnvIfPresent("GPT");
 		setEnvIfPresent("AWS_ACC_KEY");
 		setEnvIfPresent("AWS_SEC_KEY");
 		setEnvIfPresent("JWK_SECRET");
 		setEnvIfPresent("EMAIL_KEY");
+
+		System.out.println("=============================================================");
 
 		SpringApplication app = new SpringApplication(FinZenApplication.class);
 		app.setDefaultProperties(System.getProperties());
@@ -21,8 +25,9 @@ public class FinZenApplication {
 
 	private static void setEnvIfPresent(String key) {
 		String value = System.getenv(key);
-		if (value != null) {
+		if (value != null && !value.isEmpty()) {
 			System.setProperty(key, value);
+			System.out.println("✅ " + key + " definida correctamente.");
 		} else {
 			System.out.println("⚠️ Advertencia: La variable de entorno '" + key + "' no está definida.");
 		}
